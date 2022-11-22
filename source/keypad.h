f5d8def84da04acb5edade7a6073f0e304232865
Eric Reiss <reiss_eric@hotmail.com>
2022-11-22 09:25:00 -0700

    Create project

---
+++ source/keypad.h
@@ -0,0 +1,5 @@
+#ifndef __STM32L476R_NUCLEO_KEYPAD_H
+#define __STM32L476R_NUCLEO_KEYPAD_H
+char keypadPoll(void); //Set ouput data register
+void keydelay_ms(unsigned int ms);
+#endif
---
+++ source/main.c
@@ -0,0 +1,12 @@
+#include "keypad.h"
+#include "lcd.h"
+#include "stm32l476xx.h"
+
+
+/*================================================================================================
+*Author: Ash Carlsen
+*Author: Eric Reiss
+*Description: Digital FM receiver built using Nucleo L476rg board and  Sparkfun Evaluation Tuner 
+*Pins: 
+*
+===================================================================================================*/
