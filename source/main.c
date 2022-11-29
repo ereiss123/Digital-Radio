a7aaa4c77f110dbbb8c5fc833715ce76c36494ca
Ashton Carlsen <ashtoncarlsen@yahoo.com>
2022-11-29 15:35:26 -0700

    Finished copying book functions.

--- source/main.c
+++ source/main.c
@@ -77,15 +77,53 @@ void I2C_start(I2C_TypeDef * I2Cx, uint32_t DevAddress, uint8_t Size, uint8_t Di
 }
 
 void I2C_Stop(I2C_TypeDef * I2Cx){ 
-//Master: Generate STOP bit after the current byte has been transferred 
-I2Cx->CR2 |= I2C_CR2_STOP; 
-//Wait until STOPF flag is reset 
-while( (I2Cx->ISR & I2C_ISR_STOPF) == 0 ); 
-I2Cx->ICR |= I2C_ICR_STOPCF; //Write 1 to clear STOPF flag 
+	//Master: Generate STOP bit after the current byte has been transferred 
+	I2Cx->CR2 |= I2C_CR2_STOP; 
+	//Wait until STOPF flag is reset 
+	while( (I2Cx->ISR & I2C_ISR_STOPF) == 0 ); 
+	I2Cx->ICR |= I2C_ICR_STOPCF; //Write 1 to clear STOPF flag 
 }
 
 void I2C_Waitlineidle(I2C_TypeDef * I2Cx){ 
-//Wait until I2C bus is ready 
-while( (I2Cx->ISR & I2C_ISR_BUSY) == I2C_ISR_BUSY ); //If busy, wait 
+	//Wait until I2C bus is ready 
+	while( (I2Cx->ISR & I2C_ISR_BUSY) == I2C_ISR_BUSY ); //If busy, wait 
 }
 
+int8_t I2C_SendData(I2C_TypeDef *I2Cx, uint8_t SlaveAddress, uint8_t *pData, uint8_t Size) { 
+	int i; 
+	if (Size <= 0 I I pData == NULL) return -1; 
+	// Wait until the Line is idle 
+	I2C_WaitLineidle(I2Cx); 
+	// The Last argument: e = Sending data to the slave 
+	I2C_Start(I2Cx, SlaveAddress, Size, 0); 
+	for (i = 0; i < Size; i++) {  
+		// TXIS bit is set by hardware when the TXDR register is empty and the 
+		// data to be transmitted must be written in the TXDR register. It is 
+		// cleared when the next data to be sent is written in the TXDR register. 
+		// The TXIS flag is not set when a NACK is received. 
+		while( (I2Cx->ISR & I2C_ISR_TXIS) == 0 ); 
+		// TXIS is cleared by writing to the TXDR register 
+		I2Cx->TXDR = pData[i] & I2C_TXDR_TXDATA;
+	} 
+	//Wait until TC flag is set 
+	while((I2Cx->ISR & I2C_ISR_TC) == 0 && (I2Cx->ISR & I2C_ISR_NACKF) 0); 
+	if( (I2Cx->ISR & I2C_ISR_NACKF) != 0 ) 
+		return -1; 
+	I2C_Stop(I2Cx); 
+	return 0; 
+}
+
+int8_t I2C_ReceiveData(I2C_TypeDef * I2Cx, uint8_t SlaveAddress, uint8_t *pData, uint8_t Size) { 
+	int i; 
+	if (Size <= 0 I I pData == NULL) return -1; 
+	I2C_WaitLineidle(I2Cx); 
+	I2C_Start(I2Cx, SlaveAddress, Size, 1); // 1 Receiving from the slave 
+	for (i = 0; i < Size; i++) { 
+	//Wait until RXNE flag is set 
+	while( (I2Cx->ISR & I2C_ISR_RXNE) == 0 ); 
+		pData[i] = I2Cx->RXDR & I2C_RXDR_RXDATA; 
+	} 
+	while((I2Cx->ISR & I2C_ISR_TC) == 0); // Wait until TCR flag is set 
+	I2C_Stop(I2Cx); 
+	return 0; 
+}
