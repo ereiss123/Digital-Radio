594fc1a87351217c5f343f3c280298c1a685b789
Ashton Carlsen <ashtoncarlsen@yahoo.com>
2022-11-29 12:13:02 -0700

    Wrote pin numbers.

--- source/main.c
+++ source/main.c
@@ -8,5 +8,17 @@
 *Author: Eric Reiss
 *Description: Digital FM receiver built using Nucleo L476rg board and  Sparkfun Evaluation Tuner 
 *Pins: 
+*	KEYPAD:
+*		COLS: B8-11
+*		ROWS: B4-7
+*	LCD:
+*		DATA: B0-4
+*		EN:	C10
+*		RW: C11
+*		RS: C12
+*	I2C:
+*		SDL: C1 Alternate function 4- I2C3
+*		SCL: C0 Alternate Function 4- I2C3
+*	
 *
 ===================================================================================================*/
