e39d65609f467cf92589dad3c641f43acf2e6247
Ashton Carlsen <ashtoncarlsen@yahoo.com>
2022-12-06 14:45:47 -0700

    fixed switch statement in keypad.c

--- source/keypad.c
+++ source/keypad.c
@@ -32,22 +32,22 @@ char keypadPoll(void){
                 case 7: key = '1'; break;//0b0111 char 1
                 case 11:key = '2'; break;//0b1011 char 2
                 case 13:key = '3'; break;//0xD char 3
-                case 14:key = '\0'; break;//0XE char ''
+                case 14:key = 'A'; break;//0XE char 'A'
 
                 case 17:key = '4'; break;//char 4
                 case 21:key = '5'; break;//char 5
                 case 23:key = '6'; break;//char 6
-                case 24:key = '\0'; break;//char ''
+                case 24:key = 'B'; break;//char 'B'
 
                 case 27:key = '7'; break;//char 7
                 case 31:key = '8'; break;//char 8
                 case 33:key = '9'; break;//char 9
-                case 34:key = '\0'; break;//char ''
+                case 34:key = 'C'; break;//char 'C'
 
-                case 37:key = '\0'; break;//char *
+                case 37:key = '*'; break;//char *
                 case 41:key = '0'; break;//char 0
                 case 43:key = '#'; break;//char #
-                case 44:key = '\0'; break;//char ''
+                case 44:key = 'D'; break;//char 'D'
                 default: break;
             }
         }
